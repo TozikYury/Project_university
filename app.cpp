05619578f3ba86e6dedd36a545ec37ec78dcc4e3
Tozik Yury <urijtozik@gmail.com>
2024-09-28 17:12:28 +0300

    Добавление формулы Факрориала

--- app.cpp
+++ app.cpp
@@ -12,13 +12,24 @@ int pow(int number, int pow)
     return result;
 }
 
+int factorial(int number)
+{
+    int result = 1;
+
+    for (int i = 1; i <= number; i++)
+        result *= i;
+
+    return result;
+}
+
 int main(int argc, char const *argv[])
 {
     int number;
 
     cout << "Введите число" << endl;
     cin >> number;
-    cout << "Число в степени 3 =" << pow(number, 3)<<endl;
-
+    cout << "Число в степени 3 =" << pow(number, 3) << endl;
+    cout << "Fac = " << factorial(240) << endl;
+    
     return 0;
 }
